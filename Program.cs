c64a43c7b0834565373c16f25d1d07952b51eb32
WederLino <wederlinodesouza@gmail.com>
2021-05-24 21:46:25 -0300

    Para teste

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OlaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo!");
+        
+            Console.WriteLine("Tecle ENTER para finalizar");
+            Console.ReadKey();
+        }
+    }
+}
